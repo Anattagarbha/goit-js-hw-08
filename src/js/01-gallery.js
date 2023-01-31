904ca6e10a09265857e98b6cd6f62b91fcc5eee9
Anattagarbha <ruslanvic@gmail.com>
2023-01-31 22:46:03 +0200

    import lib, edit gallery script

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,43 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// create markup
+
+function createMarkup(items) {
+  return items
+    .map(
+      item =>
+        `<div>
+          <a class="gallery__link" href="${item.original}">
+                      <img 
+                      class="gallery__image"
+                      src="${item.preview}"
+                      alt="${item.description}">
+                  </a>
+          </div>`
+    )
+    .join('');
+}
+
+// select node
+
+const imgGallery = document.querySelector('.gallery');
+
+// insert markup into DOM
+
+const galleryMarkup = createMarkup(galleryItems);
+imgGallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
